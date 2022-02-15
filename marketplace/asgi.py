e86e5054491fac9830427996b017fea8cb3ba56e
Julian Amaya <julian.amayad@gmail.com>
2022-02-15 08:22:29 -0500

    Install Django 3.2 and configure psql connection

--- marketplace/asgi.py
+++ marketplace/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for marketplaceApi project.
+ASGI config for marketplace project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplaceApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
 
 application = get_asgi_application()
